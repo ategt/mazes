710188d42bff51749c3ef95916b70903d18df4d1
Adam Tegtmeier <ategtmei@gmail.com>
2018-11-15 18:50:12 -0500

    Readme now has text about the project. 'more_mazes.py' renamed to 'maze_ui.py'.

--- maze_ui.py
+++ maze_ui.py
@@ -9,6 +9,19 @@ import os
 def _cast_to_lists(narray):
     return [[int(cell) for cell in row] for row in narray]
 
+def _find_far_column(maze):
+    return len(maze[0])*2-2
+
+def _find_far_row(maze):
+    return len(maze)*2-2
+
+_corners = {"top right":    lambda maze: (_find_far_column(maze), 1),
+            "bottom right": lambda maze: (_find_far_column(maze), _find_far_row(maze)),
+            "bottom left":  lambda maze: (1, _find_far_row(maze))}
+
+def _determine_complexity(solution):
+    return Counter([cell for row in solution for cell in row]).get(1)
+
 def load_maze(path):
     with open(path,'r') as handle:
         return json.load(handle)
@@ -20,11 +33,14 @@ def save_maze(path, maze):
         with open(path,'w') as handle:
             json.dump(_cast_to_lists(maze), handle)
 
-def display_maze(maze, in_console = False):
+def display_maze(maze, in_console = False, solution = None):
     if in_console:
-        mazes.display_window(maze)
-    else:
         mazes.display_console(maze)
+    else:
+        if solution:
+            mazes.display_map(solution)
+        else:
+            mazes.display_window(maze)
 
 def generate_maze(threshold=800, passes=20):
     """ 
@@ -34,28 +50,32 @@ def generate_maze(threshold=800, passes=20):
 
         Threshold is the minimum complexity score.
         Passes is the maximum number of times that the maze can be regenerated.
+
+        returns a (maze, solution) tuple
     """
     best_sol = None
     maze = None
     for i in range(passes):
         maze = mazes.build_maze(height=20, width=30)
 
-        optionals = {}
+        goal_name_location_tuples_list = [ (key, value(maze) ) for key, value in _corners.items()]
 
-        solutiona = mazes.solve_maze(maze, endX=len(maze[0])*2-2, endY=1)
-        solutionb = mazes.solve_maze(maze, endX=len(maze[0])*2-2, endY=len(maze)*2-2)
-        solutionc = mazes.solve_maze(maze, endX=1, endY=len(maze)*2-2)
+        names_solutions_list = [ (name, mazes.solve_maze(maze, endX=location[0], endY=location[1])) for name, location in goal_name_location_tuples_list]
+        names_solutons_complexity_list = [ (name, solution, _determine_complexity(solution)) for name, solution in names_solutions_list ]
 
-        dxa = Counter([cell for row in solutiona for cell in row])
-        dxb = Counter([cell for row in solutionb for cell in row])
-        dxc = Counter([cell for row in solutionc for cell in row])    
+        best_goal, best_solution, highest_complexity = max(names_solutons_complexity_list, key=lambda item:item[2])
 
-        complexity = max(list(map(lambda ct: ct.get(1), [dxa, dxb, dxc])))
-        if complexity > threshold:
-            print(complexity)
-            pairs = [{k: v} for k, v in zip(map(lambda ct: ct.get(1), [dxa, dxb, dxc]), [solutiona, solutionb, solutionc])]
-            best_sol = reduce(lambda acc, item: {**item, **acc}, pairs, {})
-            mazes.display_map(best_sol[complexity])
-            return maze
+        if highest_complexity > threshold:
+            print(f"Start in the top left corner and go to the {best_goal}.")
+            print(f"Complexity Score: {highest_complexity}")
+            return maze, best_solution
             
     print("Passes exceeded.")
+
+def main():
+    maze, solution = generate_maze(passes = 60)
+    display_maze(maze)
+    display_maze(maze, solution = solution)
+
+if __name__ == '__main__':
+    main()
---
+++ readme.txt
@@ -0,0 +1,7 @@
+mazes
+
+This was a project that I did as part of a challenge from a friend.
+
+Running maze_ui.py will print starting and ending locations for a maze, then display the maze and then that mazes' solution.
+
+Maze displaying requires matplotlib.
